1f9b950bc3b460c1c1781904d9fc61c3ac78dc81
zoobestik <kb.chernenko@gmail.com>
2017-08-10 22:59:26 +0300

    more help descs

--- src/main/kotlin/me/telegram/getplaybot/challenge/i18n/index.kt
+++ src/main/kotlin/me/telegram/getplaybot/challenge/i18n/index.kt
@@ -2,5 +2,5 @@ package me.telegram.getplaybot.challenge.i18n
 
 fun welcome(name: String, body: String) =
     "Добро пожаловать, *$name*!\n\n" +
-        "Ты можешь поуправлять мной отправляя следующие команды:\n\n" +
+        "Ты можешь поуправлять отправляя следующие команды:\n" +
         body
